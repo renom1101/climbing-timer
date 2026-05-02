5805b00ae234924e0609601913629c04a976fc72
Renat Zaicev <zairenas@gmail.com>
2026-05-02 09:49:46 +0300

    make timer size fit the screen

--- src/components/TimerDisplay.tsx
+++ src/components/TimerDisplay.tsx
@@ -5,12 +5,22 @@ type Props = {
 };
 
 const TimerDisplay = ({ timeLeft }: Props) => {
+  const timeText = formatTime(timeLeft);
+  const timeLength = Math.max(1, timeText.length);
+  const monoCharWidthRatio = 0.62;
+  const fontSize = `calc(95vw / ${timeLength} / ${monoCharWidthRatio})`;
+
   return (
     <div>
       <div className="flex justify-center">
-        <h1 className="font-montserrat-mono font-semibold text-[35vw] text-text">
-          {formatTime(timeLeft)}
-        </h1>
+        <div className="w-[95vw]">
+          <h1
+            className="w-full whitespace-nowrap text-center font-montserrat-mono font-semibold leading-none text-text tabular-nums"
+            style={{ fontSize }}
+          >
+            {timeText}
+          </h1>
+        </div>
       </div>
     </div>
   );
